a358c7c08b8d681bf18d390f5662d2bc337e6af7
Adam <adam8690@hotmail.co.uk>
2017-03-09 16:52:32 +0000

    created class files and made interface between blackjack and game and started code to generate deck.

---
+++ BlackJack.java
@@ -0,0 +1,9 @@
+public class BlackJack implements Game{
+
+public String play(){
+  // run game methods here
+  return "This will eventually run the game";
+}  
+
+
+}
---
+++ Deck.java
@@ -0,0 +1,14 @@
+public class Deck{
+private ArrayList<Cards> cards;
+
+public Deck(Arraylist<HashMap<card>> cards);
+  this.cards = cards
+
+public Arraylist<HashMap<card>> generateDeck(){
+  suitsArray = new 
+  for 
+}
+
+
+
+}
